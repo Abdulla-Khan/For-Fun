7a77ba5279c276712ffc7980af88bd7591df0655
Muhammad Abdullah Khan <abdullahkhan.smiu@gmail.com>
2022-02-02 02:20:06 +0500

    first commit

---
+++ todo/serializers.py
@@ -0,0 +1,7 @@
+from statistics import mode
+from rest_framework import serializers
+from .models import Todo
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = "__all__"
