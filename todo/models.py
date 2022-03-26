d8f3259810352afcefa781f2d392236b5f2e8504
Muhammad Abdullah Khan <abdullahkhan.smiu@gmail.com>
2022-03-26 14:56:00 +0500

    Changes

--- todo/models.py
+++ todo/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Todo(models.Model):
-    title = models.CharField(max_length=20,null=True)
-    image = models.CharField(max_length=2000,null=True)
+    title = models.CharField(max_length=200,null=True)
     def __str__(self):
-        return self.title   
+        return self.title
