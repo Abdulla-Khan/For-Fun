95ca5461b09bb49ba50ca72b1a0abb8350816ae6
Muhammad Abdullah Khan <abdullahkhan.smiu@gmail.com>
2022-03-26 14:53:31 +0500

    Changes

--- todo/models.py
+++ todo/models.py
@@ -5,4 +5,4 @@ class Todo(models.Model):
     title = models.CharField(max_length=20,null=True)
     image = models.CharField(max_length=2000,null=True)
     def __str__(self):
-        return self.title
+        return self.title   
